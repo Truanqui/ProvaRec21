c06fe543a6237749531980695f93dc6d90993987
Johann <johannteixeira2@gmail.com>
2022-02-01 22:15:49 -0300

    Backend start

---
+++ Backend/src/main/java/com/Recuperacao/Johann/JohannApplication.java
@@ -0,0 +1,13 @@
+package com.Recuperacao.Johann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JohannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JohannApplication.class, args);
+	}
+
+}
